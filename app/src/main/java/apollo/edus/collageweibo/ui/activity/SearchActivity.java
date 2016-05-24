528aef799b7006ccef1e66d2c760d235d2d5ec29
yqpan <yongqiang.pan@dewmobile.net>
2016-05-24 14:51:10 +0800

    yqpan:  完成搜索fragment的ui展示以及点击效果

---
+++ app/src/main/java/apollo/edus/collageweibo/ui/activity/SearchActivity.java
@@ -0,0 +1,21 @@
+package apollo.edus.collageweibo.ui.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by panyongqiang on 16/5/24.
+ */
+public class SearchActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //search --> result
+        //click show user detail
+        //detail show relationShip
+        //this is OK
+        //界面上不提供操作，只有点击进入详情后，才能添加删除关注的操作
+    }
+
+}
--- app/src/main/java/apollo/edus/collageweibo/ui/fragment/DiscoverFragment.java
+++ app/src/main/java/apollo/edus/collageweibo/ui/fragment/DiscoverFragment.java
@@ -1,22 +1,45 @@
 package apollo.edus.collageweibo.ui.fragment;
 
+import android.app.SearchableInfo;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import apollo.edus.collageweibo.R;
+import apollo.edus.collageweibo.ui.activity.SearchActivity;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by panyongqiang on 16/5/20.
  */
 public class DiscoverFragment extends EsBaseFragment {
 
+    @BindView(R.id.tv_search)
+    TextView etSearch;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.discoverfragment_layout, container, false);
+        View view = inflater.inflate(R.layout.discoverfragment_layout, container, false);
+        return view;
     }
-    
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ButterKnife.bind(this, view);
+    }
+
+    @OnClick(R.id.tv_search)
+    public void onClick() {
+        startActivity(new Intent(getActivity(), SearchActivity.class));
+    }
 }
