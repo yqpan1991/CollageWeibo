9407f93e9f65a37d6c4548cd5f13ec1072461267
yqpan1991 <shifengzhongdexiao@163.com>
2016-05-31 09:00:33 +0800

    yqpan:  添加地理位置的显示

--- app/src/main/java/apollo/edus/collageweibo/ui/activity/IdeaActivity.java
+++ app/src/main/java/apollo/edus/collageweibo/ui/activity/IdeaActivity.java
@@ -20,6 +20,10 @@ import android.widget.Toast;
 
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.Poi;
+import com.baidu.location.service.LocationService;
 
 import java.util.ArrayList;
 
@@ -30,6 +34,7 @@ import apollo.edus.collageweibo.biz.bean.WeiboResult;
 import apollo.edus.collageweibo.biz.global.EsGlobal;
 import apollo.edus.collageweibo.biz.net.api.EsApiHelper;
 import apollo.edus.collageweibo.biz.user.EsUserManager;
+import apollo.edus.collageweibo.ui.application.MyApplication;
 import apollo.edus.collageweibo.ui.imglist.ImgListAdapter;
 import apollo.edus.collageweibo.utils.FillContent;
 import apollo.edus.collageweibo.utils.ToastUtil;
@@ -58,6 +63,7 @@ public class IdeaActivity extends Activity implements ImgListAdapter.OnFooterVie
     private ImageView more_button;
     private EditText mEditText;
     private TextView mLimitTextView;
+    private TextView mLocationContent;
 
     private LinearLayout mRepostlayout;
     private ImageView repostImg;
@@ -69,6 +75,7 @@ public class IdeaActivity extends Activity implements ImgListAdapter.OnFooterVie
     private ArrayList<AlbumFolderInfo> mFolderList = new ArrayList<AlbumFolderInfo>();
     private ArrayList<ImageInfo> mSelectImgList = new ArrayList<ImageInfo>();
     private WeiboResult.WeiboInfo mStatus;
+    private LocationService mService;
 
 
     @Override
@@ -89,6 +96,7 @@ public class IdeaActivity extends Activity implements ImgListAdapter.OnFooterVie
         more_button = (ImageView) findViewById(R.id.more_button);
         mEditText = (EditText) findViewById(R.id.idea_content);
         mLimitTextView = (TextView) findViewById(R.id.limitTextView);
+        mLocationContent = (TextView) findViewById(R.id.tv_location_content);
 
         mRepostlayout = (LinearLayout) findViewById(R.id.repost_layout);
         repostImg = (ImageView) findViewById(R.id.repost_img);
@@ -106,6 +114,8 @@ public class IdeaActivity extends Activity implements ImgListAdapter.OnFooterVie
             intent.putParcelableArrayListExtra("selectedImglist", mSelectImgList);
             startActivityForResult(intent, 0);
         }
+        mService = ((MyApplication)getApplication()).locationService;
+        mService.registerListener(mListener);
     }
 
 /*    private void initAccessToken() {
@@ -377,6 +387,100 @@ public class IdeaActivity extends Activity implements ImgListAdapter.OnFooterVie
         intent.putParcelableArrayListExtra("selectedImglist", mSelectImgList);
         startActivityForResult(intent, 0);
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mService = ((MyApplication)getApplication()).locationService;
+        mService.unregisterListener(mListener);
+
+    }
+
+    /*****
+     * @see copy funtion to you project
+     * 定位结果回调，重写onReceiveLocation方法，可以直接拷贝如下代码到自己工程中修改
+     *
+     */
+    private BDLocationListener mListener = new BDLocationListener() {
+
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+//            Log.e(TAG,"onReceiveLocation:"+location.toString());
+            if (null != location && location.getLocType() != BDLocation.TypeServerError) {
+                mLocationContent.setText(location.getAddrStr());
+                StringBuffer sb = new StringBuffer(256);
+                sb.append("time : ");
+/*                *
+             * 时间也可以使用systemClock.elapsedRealtime()方法 获取的是自从开机以来，每次回调的时间；
+             * location.getTime() //是指服务端出本次结果的时间，如果位置不发生变化，则时间不变*/
+
+                sb.append(location.getTime());
+                sb.append("\nerror code : ");
+                sb.append(location.getLocType());
+                sb.append("\nlatitude : ");
+                sb.append(location.getLatitude());
+                sb.append("\nlontitude : ");
+                sb.append(location.getLongitude());
+                sb.append("\nradius : ");
+                sb.append(location.getRadius());
+                sb.append("\nCountryCode : ");
+                sb.append(location.getCountryCode());
+                sb.append("\nCountry : ");
+                sb.append(location.getCountry());
+                sb.append("\ncitycode : ");
+                sb.append(location.getCityCode());
+                sb.append("\ncity : ");
+                sb.append(location.getCity());
+                sb.append("\nDistrict : ");
+                sb.append(location.getDistrict());
+                sb.append("\nStreet : ");
+                sb.append(location.getStreet());
+                sb.append("\naddr : ");
+                sb.append(location.getAddrStr());
+                sb.append("\nDescribe: ");
+                sb.append(location.getLocationDescribe());
+                sb.append("\nDirection(not all devices have value): ");
+                sb.append(location.getDirection());
+                sb.append("\nPoi: ");
+                if (location.getPoiList() != null && !location.getPoiList().isEmpty()) {
+                    for (int i = 0; i < location.getPoiList().size(); i++) {
+                        Poi poi = (Poi) location.getPoiList().get(i);
+                        sb.append(poi.getName() + ";");
+                    }
+                }
+                if (location.getLocType() == BDLocation.TypeGpsLocation) {// GPS定位结果
+                    sb.append("\nspeed : ");
+                    sb.append(location.getSpeed());// 单位：km/h
+                    sb.append("\nsatellite : ");
+                    sb.append(location.getSatelliteNumber());
+                    sb.append("\nheight : ");
+                    sb.append(location.getAltitude());// 单位：米
+                    sb.append("\ndescribe : ");
+                    sb.append("gps定位成功");
+                } else if (location.getLocType() == BDLocation.TypeNetWorkLocation) {// 网络定位结果
+                    // 运营商信息
+                    sb.append("\noperationers : ");
+                    sb.append(location.getOperators());
+                    sb.append("\ndescribe : ");
+                    sb.append("网络定位成功");
+                } else if (location.getLocType() == BDLocation.TypeOffLineLocation) {// 离线定位结果
+                    sb.append("\ndescribe : ");
+                    sb.append("离线定位成功，离线定位结果也是有效的");
+                } else if (location.getLocType() == BDLocation.TypeServerError) {
+                    sb.append("\ndescribe : ");
+                    sb.append("服务端网络定位失败，可以反馈IMEI号和大体定位时间到loc-bugs@baidu.com，会有人追查原因");
+                } else if (location.getLocType() == BDLocation.TypeNetWorkException) {
+                    sb.append("\ndescribe : ");
+                    sb.append("网络不同导致定位失败，请检查网络是否通畅");
+                } else if (location.getLocType() == BDLocation.TypeCriteriaException) {
+                    sb.append("\ndescribe : ");
+                    sb.append("无法获取有效定位依据导致定位失败，一般是由于手机的原因，处于飞行模式下一般会造成这种结果，可以试着重启手机");
+                }
+                Log.e(TAG,"location:"+sb.toString());
+            }
+        }
+
+    };
 
 
 }
